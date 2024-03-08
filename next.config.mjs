7c6940bb2ea5fa701db1ff57e238f79e48c4d8d7
Paul Bratslavsky <paul.bratslavsky@gmail.com>
2024-03-08 09:24:47 -0600

    initial commit

---
+++ next.config.mjs
@@ -0,0 +1,27 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "1337",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "thoughtful-chickens-9661b5da26.media.strapiapp.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default async function Home() { 
+  return (
+    <React.Fragment>
+      <h1>Nothing but chickens here.</h1>
+      <p>Paul's Next.js starter</p>
+    </React.Fragment>
+  );
+}
